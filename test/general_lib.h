56cf021bff2581a412e1f50a4a93cfe7043021dd
Joao Beleza <joao.beleza7@gmail.com>
2018-12-11 13:37:32 +0000

    General Lib Updated

--- test/general_lib.h
+++ test/general_lib.h
@@ -52,10 +52,97 @@ void printBits(size_t const size, void const * const ptr){
 uint16_t MIDI_interp(uint16_t mic_input){
 
   uint16_t midi_msg;
-  // Between 0 and 100
-  if (mic_input > 0x0000 && mic_input < 0x0064){
 
+  // Microphone Value Between 0 and 100
+  if (mic_input > 0x0000 && mic_input <= 0x0064){
+    midi_msg = (0x90 << 8) | 0x00;
+    return midi_msg;
   }
+
+  // Microphone Value Between 100 and 200
+  else if (mic_input > 0x0064 && mic_input <= 0x00C8){
+    midi_msg = (0x90 << 8) | 0x01;
+    return midi_msg;
+  }
+
+  // Microphone Value Between 200 and 300
+  else if (mic_input > 0x00C8 && mic_input <= 0x012C){
+    midi_msg = (0x90 << 8) | 0x02;
+    return midi_msg;
+  }
+
+  // Microphone Value Between 300 and 400
+  else if (mic_input > 0x012C && mic_input <= 0x0190){
+    midi_msg = (0x90 << 8) | 0x03;
+    return midi_msg;
+  }
+
+  // Microphone Value Between 400 and 500
+  else if (mic_input > 0x0190 && mic_input <= 0x01F4){
+    midi_msg = (0x90 << 8) | 0x04;
+    return midi_msg;
+  }
+
+  // Microphone Value Between 500 and 600
+  else if (mic_input > 0x01F4 && mic_input <= 0x0258){
+    midi_msg = (0x90 << 8) | 0x05;
+    return midi_msg;
+  }
+
+  // Microphone Value Between 600 and 700
+  else if (mic_input > 0x0258 && mic_input <= 0x02BC){
+    midi_msg = (0x90 << 8) | 0x06;
+    return midi_msg;
+  }
+
+  // Microphone Value Between 700 and 800
+  else if (mic_input > 0x02BC && mic_input <= 0x0320){
+    midi_msg = (0x90 << 8) | 0x07;
+    return midi_msg;
+  }
+
+  // Microphone Value Between 800 and 900
+  else if (mic_input > 0x0320 && mic_input <= 0x0384){
+    midi_msg = (0x90 << 8) | 0x08;
+    return midi_msg;
+  }
+
+  // Between 900 and 1000
+  else if (mic_input > 0x0384 && mic_input <= 0x03E8){
+    midi_msg = (0x90 << 8) | 0x09;
+    return midi_msg;
+  }
+
+  // Microphone Value Between 1000 and 1023
+  else if (mic_input > 0x03E8 && mic_input <= 0x03FF){
+    midi_msg = (0x90 << 8) | 0x0A;
+    return midi_msg;
+  }
+
+  else{
+    midi_msg = 0x0000;
+    return midi_msg;
+  }
+
+}
+
+
+// TURN ON LEDS ACCORDING TO MIDI MESSAGE
+void turnLeds(uint16_t midi_msg){
+
+  // Control defines if Led turns Off or On
+  uint8_t control = (uint8_t)((midi_msg & 0xFF00) >> 8);
+
+  if (control == 0x90){
+    PORTD |= (uint8_t)((midi_msg & 0x00FF));
+    return;
+  }
+
+  else if (control == 0x80){
+    PORTD &= ~((uint8_t)(midi_msg & 0x00FF));
+    return;
+  }
+
 
 
 
